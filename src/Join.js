9895c29b3fb978b1dd5aa2d9dbe1848694ddfaf3
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-04-16 14:06:44 +0700

    Tugas, mengoneksi button google dengan akun google langsung

--- src/Join.js
+++ src/Join.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from 'react';
 import { AuthContext } from "./index";
 import * as firebase from "firebase";
 
@@ -11,49 +11,70 @@ const Join = () => {
     const handleForm = e => {
         e.preventDefault();
         firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(res => {
-            if (res.user) Auth.setLoggedIn(true);
-        })
-        .catch(e => {
-            setErrors(e.message);
-        });
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(res => {
+                if (res.user) Auth.setLoggedIn(true);
+            })
+            .catch(e => {
+                setErrors(e.message);
+            });
+        // console.log(Auth);
+        // Auth.setLoggedIn(true);
     };
+
+    //join dengan google akun
+    const joinGoogle = e => {
+        e.preventDefault();
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    }
 
     return (
         <div>
-            <h1>Join</h1>
+            <h1>Login</h1>
             <form onSubmit={e => handleForm(e)}>
                 <input
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                name="email"
-                type="email"
-                placeholder="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    name="email"
+                    type="email"
+                    placeholder="email"
                 />
                 <input
-                onChange={e => setPassword(e.target.value)}
-                name="password"
-                value={password}
-                type="password"
-                placeholder="password"
+                    onChange={e => setPassword(e.target.value)}
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="password"
                 />
                 <hr />
-                <button class="googleBtn" type="button" >
-                    <img 
-                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                    alt="logo"
+                <button class="googleBtn" type="button" onClick={joinGoogle}>
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt="logo"
                     />
                     Join With Google
                 </button>
-
                 <button type="submit">Login</button>
-
                 <span>{error}</span>
             </form>
         </div>
     );
 };
-
 export default Join;
