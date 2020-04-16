c3ed77f591d644254ae777726249078149d659fe
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-04-16 12:42:42 +0700

    membuat file firebase.config.js untuk konfigurasi firebase dg react

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCMPt6tNPhia74s3I1tpHrikWVPTW-ljO4",
+    authDomain: "nurcahya-reactjs.firebaseapp.com",
+    databaseURL: "https://nurcahya-reactjs.firebaseio.com",
+    projectId: "nurcahya-reactjs",
+    storageBucket: "nurcahya-reactjs.appspot.com",
+    messagingSenderId: "261562313524",
+    appId: "1:261562313524:web:2a2dcd8f86a2778c62d361",
+    measurementId: "G-VLVSE7R33S"
+  };
